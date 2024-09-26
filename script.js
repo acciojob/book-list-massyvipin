e50dbbbd33f9ecc482ae8b3ec504f645843c518e
massyvipin <166917148+massyvipin@users.noreply.github.com>
2024-09-26 21:46:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-const titleInput = document.getElementById('title');
-const authorInput = document.getElementById('author');
-const isbnInput = document.getElementById('isbn');
-const button = document.getElementById('submit');
-const bookList = document.getElementById('book-list');
+const titleInput = document.getElementById("title");
+      const authorInput = document.getElementById("author");
+      const isbnInput = document.getElementById("isbn");
+      const submitBtn = document.getElementById("submit");
+      const bookList = document.getElementById("book-list");
 
-button.addEventListener('click',(event)=>{
-	 event.preventDefault();
+      submitBtn.addEventListener("click", (event) => {
+        event.preventDefault();
 
-const title = titleInput.value;
-const author = authorInput.value;
-const isbn = isbnInput.value;
+        const title = titleInput.value;
+        const author = authorInput.value;
+        const isbn = isbnInput.value;
 
-	if(title === "" || author === "" || isbn=== ""){
-		alert("Please fill all the fields");
+        if (title === "" || author === "" || isbn === "") {
+          alert("Please fill in all fields");
           return;
-	}
+        }
 
-	const row = document.createElement("tr");
+        const row = document.createElement("tr");
 
         row.innerHTML = `
 		<td>${title}</td>
 		<td>${author}</td>
 		<td>${isbn}</td>
 		<td><button class="delete">X</button></td>
-		`;
+	`;
 
         bookList.appendChild(row);
 
@@ -38,8 +38,5 @@ const isbn = isbnInput.value;
               bookList.removeChild(row);
             }
           }
-			
         });
-
-	
-});
+      });
